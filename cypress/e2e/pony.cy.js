f804ec3b048a47f73c4219185d253788c6875ecd
Elizaveta Sidorova <130683262+GingerMino@users.noreply.github.com>
2023-04-25 23:49:40 +0300

    Update pony.cy.js

--- cypress/e2e/pony.cy.js
+++ cypress/e2e/pony.cy.js
@@ -1,13 +1,15 @@
 describe('Проверка работоспособности оформления заказа', function () {
-    
+   Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
     it('Оформление заказа', function () {
-        cy.visit('https://huntingpony.com/')
-        cy.wait(10)
+        cy.visit('https://huntingpony.com/');
         cy.get('[data-index="2"] > .header__collections-controls > .header__collections-link').click()
         cy.get('[data-product-id="338935607"] > .product-preview__content > .product-preview__area-photo > .product-preview__photo > .img-ratio > .img-ratio__inner > a > :nth-child(1) > .product-preview__img-1').click()
-        cy.get('.add-cart-counter__btn').click()
-        cy.get('[data-add-cart-counter-plus=""]').click()
+        cy.get('.add-cart-counter__btn').wait(5000).click({ force: true})
+        cy.get('[data-add-cart-counter-plus=""]').wait(5000).click({ force: true})
         cy.get('.header__cart > .icon').click()
         cy.contains('Оформить заказ')
+        cy.contains('Попона "Пони-эскимони" мох')
     })
 })
